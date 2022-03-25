389af2b3fbf6e34efa8c5cc0997cb6d3ed41787d
Viral <viral.citrusbug@gmail.com>
2022-03-25 15:44:27 +0530

    Displayed Available Event Slot in JsonResponse

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.EventList.as_view(),name="home"),
-    path('event/<pk>',views.EventDetail.as_view(),name="event-detail"),
-    path('slot/<str:date>',views.EventSlotView.as_view(),name='event-slot')
+    path('event/<pk>',views.EventDetail.as_view(),name="event-detail")
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,3 @@
-from asyncio.windows_events import NULL
-from re import T
-from this import d
-from django.http import HttpResponse,JsonResponse
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Q
@@ -33,29 +28,6 @@ class EventDetail(APIView):
         except Event.DoesNotExist:
             return False
         
-    # def get(self,request,pk):
-    #     if self.get_event(pk):
-    #         events = Event.objects.filter(pk=pk)
-    #         dates = events.first().eventdate_set.all()
-    #         data = []
-    #         slot = []
-    #         available_slot=[]
-    #         dates_filter = {}
-    #         for date in dates:
-    #             if date not in dates_filter:
-    #                 time_slot = EventSlot.objects.filter(event=events.first(),date=date)
-    #                 for i in set(time_slot):
-    #                     slot = Time.objects.exclude(time=str(i))
-    #                     for j in set(slot):
-    #                         t = (TimeSerializer(j).data).get('time')
-    #                         available_slot.append(t)
-    #                 data.append({"date":(EventDateSerializer(date).data).get('date'),
-    #                              "available_slot":sorted(set(available_slot))
-    #                              })
-    #                 dates_filter[date] = True
-    #         return Response(data)
-    #     else:
-    #         return Response("Event Not Found")
     def get(self,request,pk):
         if self.get_event(pk):
             events = Event.objects.filter(pk=pk)
@@ -128,20 +100,3 @@ class EventDetail(APIView):
             return Response(response)
         else:
             return Response("Event Not Found")
-
-class EventSlotView(APIView):
-    def get(self,request,date):
-        time = EventSlot.objects.filter(Q(time__time__range=["00:00:00","05:00:00"]))
-        data = {}
-        date_1 = []
-        time_1 = []
-        serialize = EventSlotSerializer(time,many=True)
-        for i in serialize.data:
-            event_date = EventDateSerializer(EventDate.objects.get(pk=list(i.values())[1]))
-            event_time = TimeSerializer(Time.objects.get(pk=list(i.values())[2]))
-            date_1.append(event_date.data)
-            time_1.append(event_time.data)
-            data['date']=date_1
-            data['time']=time_1
-        print(data)
-        return Response(data)
